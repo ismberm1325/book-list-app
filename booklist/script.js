137b92a23051d2d93843b9147c2fa3f7e1c313c0
ismberm1325 <98594577+ismberm1325@users.noreply.github.com>
2022-03-02 17:22:34 +0600

    Update script.js

--- booklist/script.js
+++ booklist/script.js
@@ -24,8 +24,8 @@ class Book {
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        
-        
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+       
       `;
   
       list.appendChild(row);
