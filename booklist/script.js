8f6441f1f45a361f3b17f6e85adddac3b06e7081
ismberm1325 <98594577+ismberm1325@users.noreply.github.com>
2022-03-02 17:27:40 +0600

    Update script.js

--- booklist/script.js
+++ booklist/script.js
@@ -24,18 +24,18 @@ class Book {
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        
        
       `;
   
       list.appendChild(row);
     }
   
-    static deleteBook(el) {
+    /*static deleteBook(el) {
       if(el.classList.contains('delete')) {
         el.parentElement.parentElement.remove();
       }
-    }
+    }*/
   
     static showAlert(message, className) {
       const div = document.createElement('div');
@@ -133,3 +133,5 @@ class Book {
     //Сообщение  "Успешно удалена книга"
     UI.showAlert('Book Removed', 'success');
   });
+
+alert("Добавленную книгу сможете удалить в 12 задании :)");
